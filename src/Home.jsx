24052031e1cec7522469537dfa4a16980d37f9e9
williamsimms <williamsimms193@gmail.com>
2020-08-26 17:21:02 -0400

    styling

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Navbar from './Navbar'
 import FormatListNumberedIcon from '@material-ui/icons/FormatListNumbered'
+import { Button } from '@material-ui/core'
 import './Home.scss'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
@@ -10,6 +12,11 @@ export default function Home() {
       <div className='home__content'>
         <FormatListNumberedIcon className='home__icon' />
         <h1>To Do List</h1>
+        <Link to='/todo'>
+          <Button variant='contained' className='home__button'>
+            Use To Do List
+          </Button>
+        </Link>
       </div>
     </div>
   )
