e66246c379af96a2fd653a54216cc0c32c0d9354
williamsimms <williamsimms193@gmail.com>
2020-08-26 13:44:44 -0400

    added mixins file

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { List, ListItem, ListItemText } from '@material-ui/core'
 
 export default function ToDo({ text }) {
   return (
     <div className='todo'>
-      <li>{text}</li>
+      <List>
+        <ListItem>
+          <ListItemText primary='To Do' secondary={text} />
+        </ListItem>
+      </List>
     </div>
   )
 }
