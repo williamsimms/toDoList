68f0429d0f77a42865161f25422479fa05a1b111
williamsimms <williamsimms193@gmail.com>
2020-08-26 11:46:02 -0400

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import './App.scss'
 
 function App() {
   const [todos, setTodos] = useState(['Take Dogs For A Walk', 'Take Cats For A Walk'])
+  const [input, setInput] = useState('')
+  const addTodo = () => {
+    setTodos((todos) => [input, ...todos])
+  }
+
   return (
     <div className='App'>
       <h1 className='hero-text'>To Do List</h1>
-      <input type='text' name='' id='' />
-      <button>Add Todo </button>
+      <input type='text' name='todo' value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={addTodo}>Add Todo </button>
       <ul>
         {todos.map((todo) => (
           <li>{todo}</li>
