01a91d526f32048acc67022f402c2dba6f2345ac
williamsimms <williamsimms193@gmail.com>
2020-08-26 15:24:08 -0400

    added edit function

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -1,15 +1,33 @@
 import React, { useState, Fragment } from 'react'
-import { List, ListItem, ListItemText, Button, Modal } from '@material-ui/core'
+import { List, ListItem, ListItemText, Button, Modal, makeStyles } from '@material-ui/core'
 import database from './firebase'
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: '400',
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}))
+
 export default function ToDo({ todo }) {
+  const classes = useStyles()
   const [open, setOpen] = useState(false)
+  const [input, setInput] = useState('')
+
+  const updateTodo = () => {
+    setOpen(false)
+  }
   return (
     <Fragment>
       <Modal open={open} onClose={(e) => setOpen(false)}>
-        <div>
+        <div className={classes.paper}>
           <h1>Modal</h1>
-          <button onClick={() => setOpen(false)}>Close</button>
+          <input type='text' value={input} onChange={(e) => setInput(e.target.value)} />
+          <Button onClick={() => setOpen(false)}>Update To Do</Button>
         </div>
       </Modal>
       <List className='todo'>
