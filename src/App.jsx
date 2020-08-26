dd319c1d78f260b08a3e4577973f14f4caa014a5
williamsimms <williamsimms193@gmail.com>
2020-08-26 09:56:26 -0400

    additions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.scss'
 
 function App() {
+  const [todos, setTodos] = useState(['Take Dogs For A Walk', 'Take Cats For A Walk'])
   return (
     <div className='App'>
-      <h1>William Simms</h1>
+      <h1 className='hero-text'>To Do List</h1>
+      <input type='text' name='' id='' />
+      <button>Add Todo </button>
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   )
 }
